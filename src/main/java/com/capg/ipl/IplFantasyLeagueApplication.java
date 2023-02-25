b67e8c2d65a6124f406b56433aa53ad8f4328d76
sankarik <sankarik@inlen8520068262.corp.capgemini.com>
2023-02-25 22:00:38 +0530

    Ipl

--- src/main/java/com/capg/ipl/IplFantasyLeagueApplication.java
+++ src/main/java/com/capg/ipl/IplFantasyLeagueApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-
 @EnableJpaRepositories("com.capg.*")
 @ComponentScan("com.capg.*")
 @EntityScan("com.capg.*")
