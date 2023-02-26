15474963414ec35b7f5141418b3959d4ac7bab58
sankarik <sankarik@inlen8520068262.corp.capgemini.com>
2023-02-26 10:24:43 +0530

    Ipl

--- src/main/java/com/capg/ipl/IplFantasyLeagueApplication.java
+++ src/main/java/com/capg/ipl/IplFantasyLeagueApplication.java
@@ -2,11 +2,16 @@ package com.capg.ipl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//@EnableJpaRepositories("com.capg.*")
-//@ComponentScan("com.capg.*")
-//@EntityScan("com.capg.*")
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+@EnableJpaRepositories("com.capg.*")
+@ComponentScan("com.capg.*")
+@EntityScan("com.capg.*")
 @SpringBootApplication
-//@EnableSwagger2
+@EnableSwagger2
 public class IplFantasyLeagueApplication {
 
 	public static void main(String[] args) {
